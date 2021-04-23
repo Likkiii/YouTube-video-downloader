aa3d062499b1b196bccfe1096662334d3ff0f2e4
Likhit Ajeesh <76266365+Likkiii@users.noreply.github.com>
2021-04-24 03:42:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ from pytube import YouTube
 from pytube.cli import on_progress
 
 try:
-    #Enter video link
+    # Enter the video link
     link = input("Enter the link: ") 
     yt = YouTube(link, on_progress_callback = on_progress)
     
-    #Details of the video
+    # Details of the video
     print("Title: ", yt.title)
     print("Number of views: ", yt.views)
     print("Length of video: ", yt.length,"seconds")
 
-    #Getting the highest resolution possible
+    # Getting the highest resolution possible
     ys = yt.streams.get_highest_resolution()
 
 except:
-    #Raise error if invalid link entered
+    # Raise error if invalid link entered
     print("Invalid URL")
 
-#Downloading the video
+# Downloading the video
 try:
     print("\nProgress: ")
-    ys.download("YOUR_PATH")  #Enter your path
+    ys.download("YOUR_PATH")  # Enter your path
     print("\nSuccessfully Downloaded...")
 except: 
     print("\nCouldn't download video")
